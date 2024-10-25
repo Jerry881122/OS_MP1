b1debb6bb8440dbfc290b4e049e80b6ec05f3ed4
吳孟儒 <renyor@wumengrus-Air.mshome.net>
2024-10-25 21:23:09 +0800

    10_25_backup

---
+++ code/test/consoleIO_test1.c
@@ -0,0 +1,14 @@
+#include "syscall.h"
+
+int
+main()
+{
+
+	MSG("asfasdas");
+	int n;
+	for (n=15;n>5;n--) {
+		PrintInt(n);
+	}
+        Halt();
+}
+
---
+++ code/test/os_students.sh
@@ -0,0 +1,6 @@
+make clean
+# make 
+# ../build.linux/nachos -e test_funck
+
+make 
+../build.linux/nachos -e fileIO_test2
